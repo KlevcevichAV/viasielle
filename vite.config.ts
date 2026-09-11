54f16ca9f8cef53036b135a37af96f82789c2f23
C5388478 <aliaksandr.kliautsevich@sap.com>
2026-09-11 15:07:28 +0300

    Set `base` to `/` in Vite config for consistent deployment paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.GITHUB_ACTIONS ? '/viasielle/' : '/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
